5674897d47aa76d92827b388d21aa63405ed7177
abraham_paul <abrahampaul096@gmail.com>
2020-12-26 10:45:51 +0530

    Deleting Copy constructor, Copy assignment operator instead of defaulting them

--- singleton_pattern.cpp
+++ singleton_pattern.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 struct SingletonClass {
+    // Thread safe implementation of getInstance()
     static SingletonClass& getInstance(int t_value) {
         static SingletonClass obj{t_value};
         return obj;
@@ -12,13 +13,13 @@ struct SingletonClass {
 
 private:
     /* 
-        Making Constructor, Copy constructor, Copy assignment operator
+        Making Constructor private and deleting Copy constructor & Copy assignment operator 
         so that only one object exists in the program at all times.
         Move Consrtuctor is not generated because of Rule of 5.
     */
     SingletonClass(int t_value) : m_value{t_value} {} // Parameterized CTOR
-    SingletonClass(const SingletonClass&) = default; // Copy CTOR
-    SingletonClass& operator =(const SingletonClass&) = default; // Copy Assignment
+    SingletonClass(const SingletonClass&) = delete; // Copy CTOR
+    SingletonClass& operator =(const SingletonClass&) = delete; // Copy Assignment
     int m_value;
 };
 
