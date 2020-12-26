830250986b460642b901f4de441230440e58c954
abraham_paul <abrahampaul096@gmail.com>
2020-12-26 10:10:24 +0530

    Added private Copy ctor and copy assignment

--- singleton_pattern.cpp
+++ singleton_pattern.cpp
@@ -6,18 +6,24 @@ struct SingletonClass {
         static SingletonClass obj{t_value};
         return obj;
     }
-
     int getValue() const {
         return m_value;
     }
 
-    private:
-    SingletonClass(int t_value) : m_value{t_value} {}
+private:
+    /* 
+        Making Constructor, Copy constructor, Copy assignment operator
+        so that only one object exists in the program at all times.
+        Move Consrtuctor is not generated because of Rule of 5.
+    */
+    SingletonClass(int t_value) : m_value{t_value} {} // Parameterized CTOR
+    SingletonClass(const SingletonClass&) = default; // Copy CTOR
+    SingletonClass& operator =(const SingletonClass&) = default; // Copy Assignment
     int m_value;
 };
 
 int main() {
-    auto obj = SingletonClass::getInstance(20);
+    auto& obj = SingletonClass::getInstance(20);
     cout << obj.getValue() << "\n";
     return 0;
 }
